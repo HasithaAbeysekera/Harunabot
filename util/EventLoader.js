787aec2d597e26691c67bd59aaf98f63f5365c31
HasithaAbeysekera <hasithaa@hotmail.com>
2017-12-15 12:53:00 +0800

    removed unneccessary client events

--- util/EventLoader.js
+++ util/EventLoader.js
@@ -6,8 +6,6 @@ module.exports = client => {
   client.on('message', reqEvent('message'));
   client.on('guildMemberAdd', reqEvent('guildmemberadd'));
   client.on('guildMemberRemove', reqEvent('guildmemberremove'));
-  client.on('guildCreate', reqEvent('guildcreate'));
   client.on('emojiCreate', reqEvent('emojiCreate'));
   client.on('emojiUpdate', reqEvent('emojiUpdate'));
-  client.on('presenceUpdate', reqEvent('presenceUpdate'));
 };
