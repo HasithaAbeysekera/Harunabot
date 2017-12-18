88f051cd73d5e8592390ccc7abe48bfe3f4b90ed
HasithaAbeysekera <hasithaa@hotmail.com>
2017-12-18 17:04:21 +0800

    lewd command

---
+++ commands/lewd.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+var assets = require('../assets/assets.json');
+
+exports.run = function(client, message, args) {
+
+  size = assets["lewd"].length;
+  quoteNumber = Math.floor((Math.random() * size) + 1);
+  message.channel.send(`${size}`);
+
+  const embed = new Discord.RichEmbed()
+      .setImage(assets["lewd"][quoteNumber])
+      .setColor(0xFFFFFF)
+  message.channel.send(embed);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'lewd',
+  description: 'Puck approves',
+  usage: 'puckclap'
+};
