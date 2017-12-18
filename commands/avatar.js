2680a9461e64dd8d5bb1cdec8b554807ae6916ed
HasithaAbeysekera <hasithaa@hotmail.com>
2017-12-19 01:05:26 +0800

    avatar: removed server icon + text user search

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,30 +4,14 @@ const owner = require('../config.json').ownerid;
 
 exports.run = function(client, message, args) {
   if (!args[0]) {
-    message.reply("Your avatar is:");
+    message.reply("your avatar is:");
     return message.channel.send(new Discord.Attachment(message.author.avatarURL, 'avatar.jpg'));
-  } else if (args[0].toLowerCase() === 'server') {
-    serveravatar = message.guild.iconURL;
-    if (serveravatar) {
-      message.channel.send(`The server avatar is:`);
-      return message.channel.send(new Discord.Attachment(message.guild.iconURL, 'avatar.jpg'));
-    } else {
-      return message.channel.send("This server doesn't have an icon");
-    }
   } else if (message.mentions.users.first()) {
     target = message.mentions.users.first();
     message.channel.send(`${target}'s avatar is:`);
     return message.channel.send(new Discord.Attachment(target.avatarURL, 'avatar.jpg'));
-  } else if (args[0]) {
-    target = getUser(message, args[0]);
-    if (target) {
-      message.channel.send(`${target}'s avatar is:`);
-      return message.channel.send(new Discord.Attachment(target.avatarURL, 'avatar.jpg'));
-    } else {
-      return message.channel.send(`Error: User not found`);
-    }
   } else {
-    return message.channel.send(`Error: incorrect format`);
+    return message.channel.send(`Error: incorrect format. See help for further details`);
   }
 };
 
