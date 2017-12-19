31cd9a5e70dde88807c855969d3e1191a59b9f4c
HasithaAbeysekera <hasithaa@hotmail.com>
2017-12-19 14:37:19 +0800

    purge command now checks for permissions

--- commands/purge.js
+++ commands/purge.js
@@ -3,10 +3,16 @@ const prefix = require('../config.json').prefix;
 var getUser = require('../util/getUser.js');
 
 exports.run = (client, message, args) => {
+  if(!message.guild.me.hasPermission('MANAGE_MESSAGES')){
+    return message.channel.send("I require the Manage Messages permission to use this function.");
+  }
   if (!args[0]) {
     return message.channel.send("Please specify the amount of messages to delete and, if desired, a user");
   } else {
-    amount = parseInt(args[0]);
+    amount = parseInt(args[0]) + 1;
+    if(amount <= 1 || amount > 100){
+      return message.channel.send("You need to input a number between 1 and 99");
+    }
     if (!amount) {
       return message.channel.send("Error: please specify a number of messages to delete");
     } else {
@@ -16,8 +22,8 @@ exports.run = (client, message, args) => {
         }).then((messages) => {
           message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
         });
-        message.channel.send(`Deleting last ${amount} messages...`).then(msg => {
-          msg.edit(`Successfully deleted ${amount} messages!`);
+        message.channel.send(`Deleting last ${amount-1} messages...`).then(msg => {
+          msg.edit(`Successfully deleted ${amount-1} messages!`);
         });
       } else
       if (message.mentions.members.first()) {
@@ -28,24 +34,11 @@ exports.run = (client, message, args) => {
           }
           message.channel.bulkDelete(userMessages).catch(error => console.log(error.stack));
         });
-        message.channel.send(`Deleting last ${amount} messages by user mentioned: ${target}`).then(msg => {
-          msg.edit(`Successfully deleted ${amount} messages by ${target}!`);
+        message.channel.send(`Deleting last ${amount-1} messages by user mentioned: ${target}`).then(msg => {
+          msg.edit(`Successfully deleted ${amount-1} messages by ${target}!`);
         });
       } else {
-        target = getUser(message, args[1]);
-        if (!target) {
-          message.channel.send(`Error: User not found`);
-        } else {
-          message.channel.fetchMessages({}).then((messages) => {
-            if (target) {
-              userMessages = messages.filter(m => m.author.id === target.id).array().slice(0, amount);
-            }
-            message.channel.bulkDelete(userMessages).catch(error => console.log(error.stack));
-          });
-          message.channel.send(`Deleting last ${amount} messages by user mentioned: ${target}`).then(msg => {
-            msg.edit(`Successfully deleted ${amount} messages by ${target}!`);
-          });
-        }
+        message.channel.send(`Error: User not found`);
       }
     }
   }
