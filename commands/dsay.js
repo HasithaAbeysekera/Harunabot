574a52844ac854d5a90083d1454c2679b889c4a1
HasithaAbeysekera <hasithaa@hotmail.com>
2017-12-20 18:59:25 +0800

    dsay fixed

--- commands/dsay.js
+++ commands/dsay.js
@@ -1,18 +1,19 @@
-var default = require('../config.json').defaultChannel;
+const defaultChannelID = require("../config.json").defaultChannel;
 
 exports.run = function(client, message, args) {
-    return message.guild.default.send(args.join(" "));
+  defaultChannel = guild.channels.find(u => u.id == defaultChannelID);
+  return defaultChannel.send(args.join(" "));
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 2
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 2
 };
 
 exports.help = {
-    name: 'dsay',
-    description: 'Haruna will talk in the default channel',
-    usage: 'dsay [message]'
+  name: 'dsay',
+  description: 'Haruna will talk in the default channel',
+  usage: 'dsay [message]'
 };
