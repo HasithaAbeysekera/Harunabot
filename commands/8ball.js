a010535f9450414f3dbd55d90d19bb006b1a1b7c
HasithaAbeysekera <hasithaa@hotmail.com>
2018-01-03 13:00:57 +0530

    -8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,19 +6,13 @@ exports.run = function(client, message, args) {
   if (!args[0]) {
     return message.reply("aren't you going to ask a question?");
   }
-  message.channel.send(`Quote size ${eightball.length}`)
-  for (let i = 0; i < 100; i++) {
-    argstring = (Math.floor((Math.random() * 100))).toString();
-    // argstring = args.join(' ');
-    hash = farmhash.hash32(argstring);
-    hash100 = hash % 100;
-    hashFinal = Math.round(hash100 / 5);
-    message.channel.send(hashFinal);
-  }
-  message.channel.send('done');
-  // size = eightball.length;
-  // quoteNumber = Math.floor((Math.random() * size));
-  // message.channel.send(`${eightball[quoteNumber]}`);
+  // argstring = (Math.floor((Math.random() * 100))).toString();
+  argstring = args.join(' ');
+  hash = farmhash.hash32(argstring);
+  hash100 = hash % 100;
+  hashFinal = Math.round(hash100 / 5);
+  // message.channel.send(hashFinal);
+  message.channel.send(`${eightball[hashFinal]}`);
 };
 
 exports.conf = {
