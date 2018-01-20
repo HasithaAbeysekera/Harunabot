263be8be00e13ebd3104fc04656cc99c554a37c0
HasithaAbeysekera <hasithaa@hotmail.com>
2018-01-20 12:06:41 +0530

    added log channel for member add/remove

--- events/guildmemberadd.js
+++ events/guildmemberadd.js
@@ -1,7 +1,10 @@
 const defaultChannelID = require("../config.json").defaultChannel;
+const logChannelID = require("../config.json").logChannel;
 module.exports = member => {
   let guild = member.guild;
   defaultChannel = guild.channels.find(u => u.id == defaultChannelID);
+  logChannel = guild.channels.find(u => u.id == logChannelID);
   console.log(`${member.user.username} has joined ${guild}`);
   defaultChannel.send(`${member.user} has joined the server, welcome!`);
+  logChannel.send(`${member.user} has joined the server, welcome!`);
 }
--- events/guildmemberremove.js
+++ events/guildmemberremove.js
@@ -1,7 +1,10 @@
 const defaultChannelID = require("../config.json").defaultChannel;
+const logChannelID = require("../config.json").logChannel;
 module.exports = member => {
   let guild = member.guild;
   defaultChannel = guild.channels.find(u => u.id == defaultChannelID);
+  logChannel = guild.channels.find(u => u.id == logChannelID);
   console.log(`${member.user.username} has left ${guild}`);
   defaultChannel.send(`${member.user} has left the server :cry:`);
+  logChannel.send(`${member.user} has left the server :cry:`);
 }
