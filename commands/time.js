7fbed97b475dce043f343061102f6dbda7a598bd
HasithaAbeysekera <hasithaa@hotmail.com>
2018-03-05 19:54:45 +0800

    updated quotes

--- commands/time.js
+++ commands/time.js
@@ -4,19 +4,22 @@ let moment = require('moment-timezone');
 
 exports.run = function(client, message, args) {
 
-let timeINDIA = moment().tz("Asia/Kolkata").format('HH:mm');
-let timeINDO = moment().tz("Asia/Jakarta").format('HH:mm');
-let timeSG = moment().tz("Asia/Singapore").format('HH:mm');
-let timeSA = moment().tz("Australia/Adelaide").format('HH:mm');
-let timeMELB = moment().tz("Australia/Melbourne").format('HH:mm');
+  if (!args[0]) {
+    let timeINDIA = moment().tz("Asia/Kolkata").format('HH:mm');
+    let timeINDO = moment().tz("Asia/Jakarta").format('HH:mm');
+    let timeSG = moment().tz("Asia/Singapore").format('HH:mm');
+    let timeSA = moment().tz("Australia/Adelaide").format('HH:mm');
+    let timeMELB = moment().tz("Australia/Melbourne").format('HH:mm');
 
 
-return message.channel.send(`It is currently:\n\n` +
-  `${timeINDIA} in India\n` +
-  `${timeINDO} in Indonesia\n` +
-  `${timeSG} in Singapore/HK/Philippines\n` +
-  `${timeSA} in South Australia\n` +
-  `${timeMELB} in Melbourne`);
+    return message.channel.send(`It is currently:\n\n` +
+      `${timeINDIA} in India\n` +
+      `${timeINDO} in Indonesia\n` +
+      `${timeSG} in Singapore/HK/Philippines\n` +
+      `${timeSA} in South Australia\n` +
+      `${timeMELB} in Melbourne`);
+  }
+
 };
 
 exports.conf = {
