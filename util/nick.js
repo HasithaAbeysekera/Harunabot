871ceb451b197de4dbe2f018f6021bf4b8b457b8
HasithaAbeysekera <hasithaa@hotmail.com>
2018-03-31 18:45:16 +0800

    final aprilfools

--- util/nick.js
+++ util/nick.js
@@ -1,11 +1,11 @@
 module.exports = (client) => {
-    var memberList = client.guilds.first().members.array();
-    let names = ["Shimarin", "HolotheWise", "WongSongMong", "AllenCantCount", "TrapsAreGay", "Poi", "REEEEEEEEEEEEEEEEE", "LoliLover"];
-    for (i = 0; i < memberList.length; i++) {
-      if (!memberList[i].roles.find(u => u.name == "Admirals")) {
-        var choice = Math.floor((Math.random() * names.length));
-        memberList[i].setNickname(names[choice]);
-      }
+  var memberList = client.guilds.first().members.array();
+  let names = ["Shimarin", "HolotheWise", "WongSongMong", "AllenCantCount", "TrapsAreGay", "Poi", "REEEEEEEEEEEEEEEEE", "LoliLover", "AquaBestGirl"];
+  for (i = 0; i < memberList.length; i++) {
+    if (!memberList[i].roles.find(u => u.name == "Admirals")) {
+      var choice = Math.floor((Math.random() * names.length));
+      memberList[i].setNickname(names[choice]);
     }
-    return console.log("done");
-  };
+  }
+  return console.log("done");
+};
