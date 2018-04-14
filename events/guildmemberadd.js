6c52308658ab79e755c39cd47e3de15cb224d390
HasithaAbeysekera <hasithaa@hotmail.com>
2018-04-14 19:48:20 +0800

    new welcome messages

--- events/guildmemberadd.js
+++ events/guildmemberadd.js
@@ -1,10 +1,16 @@
 const defaultChannelID = require("../config.json").defaultChannel;
 const logChannelID = require("../config.json").logChannel;
+let welcome = require('../assets/welcome.json');
 module.exports = member => {
   let guild = member.guild;
-  defaultChannel = guild.channels.find(u => u.id == defaultChannelID);
-  logChannel = guild.channels.find(u => u.id == logChannelID);
+  let defaultChannel = guild.channels.find(u => u.id == defaultChannelID);
+  let logChannel = guild.channels.find(u => u.id == logChannelID);
   console.log(`${member.user.username} has joined ${guild}`);
-  defaultChannel.send(`${member.user} has joined the server, welcome!`);
+  let size = welcome.length;
+  let randNumber = Math.floor((Math.random() * size));
+  let welcomeMsg = welcome[randNumber];
+  let finalWelcome = welcomeMsg.replace("xxx", member.user);
+
+  defaultChannel.send(finalWelcome);
   logChannel.send(`${member.user} has joined the server, welcome!`);
 }
