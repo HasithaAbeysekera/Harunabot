ccb3ccd7f75ac46b9eb14858ef8b5a4b0052715a
HasithaAbeysekera <hasithaa@hotmail.com>
2018-04-18 16:43:43 +0800

    fixing Cadd

--- util/Cadd.js
+++ util/Cadd.js
@@ -4,16 +4,16 @@ module.exports = (message, client, args, team) => {
   if (!args[1] || !message.mentions.users.first()) {
     return message.channel.send(`Error: no user mentionned`);
   } else {
-    let userToAdd = message.mentions.users.first().id;
+    userToAdd = message.mentions.users.first().id;
     if (!args[2]) {
       return message.channel.send(`Error: please input points to add`);
     } else if (!parseInt(args[2])) {
       return message.channel.send(`Error: please input points to add`);
     }
     pointsToAdd = parseInt(args[2]);
-    if (!team.get(userToAdd)) {
-      return message.channel.send(`Error: invalid user`);
-    }
+    // if (!team.get(userToAdd)) {
+    //   return message.channel.send(`Error: invalid user`);
+    // }
     let newPoints = parseInt(team.get(userToAdd) + pointsToAdd);
     team.set(userToAdd, newPoints);
     return message.channel.send(`Added ${pointsToAdd} points to ${message.guild.members.find(u => u.id == userToAdd)}. Total points:${newPoints}`);
