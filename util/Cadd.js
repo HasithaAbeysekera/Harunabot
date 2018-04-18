95b033cfb6395f723f212105398c05c10c87e2dc
HasithaAbeysekera <hasithaa@hotmail.com>
2018-04-18 17:43:09 +0800

    testing Cadd3

--- util/Cadd.js
+++ util/Cadd.js
@@ -4,18 +4,18 @@ module.exports = (message, client, args, team) => {
   if (!args[1] || !message.mentions.members.first()) {
     return message.channel.send(`Error: no user mentionned`);
   } else {
-    let userToAdd = message.mentions.members.first();
+    let userToAdd = message.mentions.members.first().id;
     if (!args[2]) {
       return message.channel.send(`Error: please input points to add`);
     } else if (!parseInt(args[2])) {
       return message.channel.send(`Error: please input points to add`);
     }
     pointsToAdd = parseInt(args[2]);
-    // if (!team.get(userToAdd.id)) {
-    //   return message.channel.send(`Error: invalid user`);
-    // }
-    let newPoints = parseInt(team.get(userToAdd.id) + pointsToAdd);
-    team.set(userToAdd.id, newPoints);
-    return message.channel.send(`Added ${pointsToAdd} points to ${message.guild.members.find(u => u.id == userToAdd.id)}. Total points:${newPoints}`);
+    if (!team.has(userToAdd)) {
+      return message.channel.send(`Error: invalid user`);
+    }
+    let newPoints = parseInt(team.get(userToAdd) + pointsToAdd);
+    team.set(userToAdd, newPoints);
+    return message.channel.send(`Added ${pointsToAdd} points to ${message.guild.members.find(u => u.id == userToAdd)}. Total points: ${newPoints}`);
   }
 };
