81657cafc3bf8bc74b0ae1011470dd1fc332c0c0
HasithaAbeysekera <hasithaa@hotmail.com>
2018-12-11 00:20:16 +0800

    update message reaction

--- events/CWmessageReaction.js
+++ events/CWmessageReaction.js
@@ -6,11 +6,8 @@ module.exports = (messageReaction,user) => {
   }
   messageReaction.message.channel.send(`rolecall is active: ${client.rollcallActive} and id is: ${client.rollcallMsgId}`);
   if(client.rollcallActive && messageReaction.message.id == client.rollcallMsgId){
-    messageReaction.message.channel.send(`active area reached`);
     if(messageReaction.emoji.name == '🇾'){
-      messageReaction.message.channel.send(`reacted by ${user}`);
       let target = messageReaction.message.guild.members.find(u => u.id == user.id);
-      messageReaction.message.channel.send(`member found ${target.id}`);
       target.addRole(cwrole);
     }
   }
