028de41a3c63c822975c88afe627520efd610e41
HasithaAbeysekera <hasithaa@hotmail.com>
2019-01-24 00:41:12 +0800

    event removed

--- util/EventLoader.js
+++ util/EventLoader.js
@@ -4,10 +4,6 @@ module.exports = client => {
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
   client.on('message', reqEvent('message'));
-  client.on('guildMemberAdd', reqEvent('guildmemberadd'));
-  client.on('guildMemberRemove', reqEvent('guildmemberremove'));
-  client.on('emojiCreate', reqEvent('emojiCreate'));
-  client.on('emojiUpdate', reqEvent('emojiUpdate'));
   client.on('messageReactionAdd', reqEvent('CWmessageReaction'));
   client.on('messageReactionRemove', reqEvent('CWmessageUnReaction'));
   
